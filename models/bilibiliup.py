74e714fe923d342cda984a73cc8e5a9c393241f6
hz157 <horizon_157@outlook.com>
2023-11-28 08:31:34 +0800

    + 新增bilibili up主信息数据库

---
+++ models/bilibiliup.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, DateTime, JSON
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql import func
+
+Base = declarative_base()
+
+
+class BilibiliUp(Base):
+    __tablename__ = 'bilibili_up'
+
+    id = Column(Integer, primary_key=True)
+    mid = Column(Integer)
+    view = Column(Integer)
+    likes = Column(Integer)
+    following = Column(Integer)
+    follower = Column(Integer)
+    update_time = Column(DateTime, default=func.now())
