2b1b0a8aa39c8a6fdb387b1cde2643b3f82c9b78
hz157 <horizon_157@outlook.com>
2023-12-19 00:33:10 +0800

    + 用户认证相关

---
+++ models/user.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, DateTime, JSON
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql import func
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    password = Column(String)
+    last_login_time = Column(DateTime)
+    last_login_ip = Column(String)
